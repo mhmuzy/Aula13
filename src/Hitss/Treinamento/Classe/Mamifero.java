35fc947d7f1fd8ecb29956dc8a54bfb33df2c37c
marcio henrique <mhmuzy@embratel.com.br>
2021-04-27 14:01:17 -0300

    Finalização do Programa [Treinamento]

--- src/Hitss/Treinamento/Classe/Mamifero.java
+++ src/Hitss/Treinamento/Classe/Mamifero.java
@@ -2,4 +2,14 @@ package Hitss.Treinamento.Classe;
 
 public class Mamifero extends Animal {
 	protected String corPelo;
+
+	public String getCorPelo() {
+		return corPelo;
+	}
+
+	public void setCorPelo(String corPelo) {
+		this.corPelo = corPelo;
+	}
+	
+	
 }
--- src/Hitss/Treinamento/Infra/Controller.java
+++ src/Hitss/Treinamento/Infra/Controller.java
@@ -1,10 +1,15 @@
 package Hitss.Treinamento.Infra;
 
 import java.util.Scanner;
+import Hitss.Treinamento.Classe.*;
+import Hitss.Treinamento.Repositorio.*;
 
 public class Controller {
 	
 	public static void Play() {
+		try {
+			
+		
 		System.out.println("");
 		System.out.println("     Hitss Treinamento");
 		System.out.println("");
@@ -14,8 +19,6 @@ public class Controller {
 		System.out.println("(3) - Emitir o som do Mamífero");
 		System.out.println("-------------------------------------");
 		System.out.println("");
-		System.out.println("Prezado(a) Usuário(a), Estamos em Desenvolvimento no Momento.");
-		System.out.println("");
 		System.out.println("Escolha a opção desejada.");
 		System.out.println("");
 		Scanner teclado = new Scanner(System.in);
@@ -23,6 +26,15 @@ public class Controller {
 		
 		switch (opcao) {
 		case 1:
+			try {
+				
+			
+			int[] codigo = new int[5];
+			codigo[1] = 1;
+			codigo[2] = 2;
+			codigo[3] = 3;
+			codigo[4] = 4;
+			
 			float[] peso = new float[5];
 			peso[1] = 10.0f;
 			peso[2] = 7.0f;
@@ -40,10 +52,191 @@ public class Controller {
 			membros[2] = 4;
 			membros[3] = 4;
 			membros[4] = 4;
+			
+			String[] corPelo = new String[5];
+			corPelo[1] = "Preto malhado Amarelo";
+			corPelo[2] = "Branco";
+			corPelo[3] = "Marrom";
+			corPelo[4] = "Preto malhado Vermelho";
+			
+			System.out.println("");
+			System.out.println("Digite o Código do Cachorro.");
+			System.out.println("");
+			int Codigo = teclado.nextInt();
+			
+			
+			if (Codigo == codigo[Codigo]) {
+				
+				Cachorro cachorro = new Cachorro();
+				cachorro.setPeso(peso[Codigo]);
+				cachorro.setIdade(idade[Codigo]);
+				cachorro.setMembros(membros[Codigo]);
+				cachorro.setCorPelo(corPelo[Codigo]);
+				System.out.println("");
+				System.out.println("     Resultado da Busca");
+				System.out.println("-----------------------------------------");
+				System.out.println("Código do Cachorro: " +  codigo[Codigo]);
+				System.out.println("Peso: " + cachorro.getPeso());
+				System.out.println("Idade: " + cachorro.getIdade());
+				System.out.println("Quantidade de Membros: " + cachorro.getMembros());
+				System.out.println("Cor do Pelo: " + cachorro.getCorPelo());
+				System.out.println("-----------------------------------------");
+				System.out.println("");
+				CachorroRepositorio rep = new CachorroRepositorio();
+				rep.emitirSom();
+			} else {
+				System.out.println("");
+				System.out.println("Nenhum(a) Cachorro(a) Encontrado(a).");
+			}
+			} catch (Exception e) {
+				System.out.println("");
+				System.out.println("Nenhum(a) Cachorro(a) Encontrado(a).");
+			}
  			break;
-
+		case 2:
+			try {
+				
+			
+			int[] codigo = new int[5];
+			codigo[1] = 1;
+			codigo[2] = 2;
+			codigo[3] = 3;
+			codigo[4] = 4;
+			
+			float[] peso = new float[5];
+			peso[1] = 30.0f;
+			peso[2] = 40.0f;
+			peso[3] = 35.0f;
+			peso[4] = 35.0f;
+			
+			int[] idade = new int[5];
+			idade[1] = 7;
+			idade[2] = 4;
+			idade[3] = 8;
+			idade[4] = 4;
+			
+			int[] membros = new int[5];
+			membros[1] = 4;
+			membros[2] = 4;
+			membros[3] = 4;
+			membros[4] = 4;
+			
+			String[] corPelo = new String[5];
+			corPelo[1] = "Cinza";
+			corPelo[2] = "Cinza";
+			corPelo[3] = "Cinza";
+			corPelo[4] = "Cinza";
+			
+			System.out.println("");
+			System.out.println("Digite o Código do Lobo.");
+			System.out.println("");
+			int Codigo = teclado.nextInt();
+			
+			
+			if (Codigo == codigo[Codigo]) {
+				
+				Lobo lobo = new Lobo();
+				lobo.setPeso(peso[Codigo]);
+				lobo.setIdade(idade[Codigo]);
+				lobo.setMembros(membros[Codigo]);
+				lobo.setCorPelo(corPelo[Codigo]);
+				System.out.println("");
+				System.out.println("     Resultado da Busca");
+				System.out.println("-----------------------------------------");
+				System.out.println("Código do Lobo: " +  codigo[Codigo]);
+				System.out.println("Peso: " + lobo.getPeso());
+				System.out.println("Idade: " + lobo.getIdade());
+				System.out.println("Quantidade de Membros: " + lobo.getMembros());
+				System.out.println("Cor do Pelo: " + lobo.getCorPelo());
+				System.out.println("-----------------------------------------");
+				System.out.println("");
+				LoboRepositorio rep = new LoboRepositorio();
+				rep.emitirSom();
+			} else {
+				System.out.println("");
+				System.out.println("Nenhum(a) Lobo(a) Encontrado(a).");
+			}
+			} catch (Exception e) {
+				System.out.println("");
+				System.out.println("Nenhum(a) Lobo(a) Encontrado(a).");
+			}
+ 			break;
+		case 3:
+			try {
+				
+			
+			int[] codigo = new int[5];
+			codigo[1] = 1;
+			codigo[2] = 2;
+			codigo[3] = 3;
+			codigo[4] = 4;
+			
+			float[] peso = new float[5];
+			peso[1] = 3.0f;
+			peso[2] = 150.0f;
+			peso[3] = 120.0f;
+			peso[4] = 115.0f;
+			
+			int[] idade = new int[5];
+			idade[1] = 3;
+			idade[2] = 18;
+			idade[3] = 16;
+			idade[4] = 15;
+			
+			int[] membros = new int[5];
+			membros[1] = 2;
+			membros[2] = 4;
+			membros[3] = 4;
+			membros[4] = 4;
+			
+			String[] corPelo = new String[5];
+			corPelo[1] = "Preto";
+			corPelo[2] = "Marrom";
+			corPelo[3] = "Malhada";
+			corPelo[4] = "Marrom";
+			
+			System.out.println("");
+			System.out.println("Digite o Código do Mamífero.");
+			System.out.println("");
+			int Codigo = teclado.nextInt();
+			
+			
+			if (Codigo == codigo[Codigo]) {
+				
+				Mamifero mamifero = new Mamifero();
+				mamifero.setPeso(peso[Codigo]);
+				mamifero.setIdade(idade[Codigo]);
+				mamifero.setMembros(membros[Codigo]);
+				mamifero.setCorPelo(corPelo[Codigo]);
+				System.out.println("");
+				System.out.println("     Resultado da Busca");
+				System.out.println("-----------------------------------------");
+				System.out.println("Código do Mamífero: " +  codigo[Codigo]);
+				System.out.println("Peso: " + mamifero.getPeso());
+				System.out.println("Idade: " + mamifero.getIdade());
+				System.out.println("Quantidade de Membros: " + mamifero.getMembros());
+				System.out.println("Cor do Pelo: " + mamifero.getCorPelo());
+				System.out.println("-----------------------------------------");
+				System.out.println("");
+				MamiferoRepositorio rep = new MamiferoRepositorio();
+				rep.emitirSom();
+			} else {
+				System.out.println("");
+				System.out.println("Nenhum(a) Mamífero(a) Encontrado(a).");
+			}
+			} catch (Exception e) {
+				System.out.println("");
+				System.out.println("Nenhum(a) Mamífero(a) Encontrado(a).");
+			}
+ 			break;
 		default:
+			System.out.println("");
+			System.out.println("Opção inválida");
 			break;
+		}
+		} catch (Exception e) {
+			System.out.println("");
+			System.out.println("Opção inválida");
 		}
 	}
 
